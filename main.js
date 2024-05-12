441fc15e5851fb91828111be0bc606ebff51cdc4
PierreKoding <pierresitorus@gmail.com>
2024-05-13 01:43:47 +0700

    12/05/2024

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 
-var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
 
 var swiper = new Swiper(".swipe-container", {
-    spaceBetween: 20,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 2,
-        },
-        568: {
-            slidesPerView: 3,
-        },
-        768: {
-            slidesPerView: 4,
-        },
-        968: {
-            slidesPerView: 5,
-        },
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+      0: {
+          slidesPerView: 2,
+      },
+      568: {
+          slidesPerView: 3,
+      },
+      768: {
+          slidesPerView: 4,
+      },
+      968: {
+          slidesPerView: 5,
+      },
 
-    }
-  });
+  }
+});
+
+
+// BACK BUTTON
+
+const backButton = document.querySelector('.back-button');
+
+backButton.addEventListener('click', () => {
+
+window.history.back();
+});
