11239f8d228fa4d9e95ad297a44a9edfbae9491b
Skyy501 <lukemangala540@gmail.com>
2024-05-21 23:56:20 +0700

    enter

---
+++ home.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".swipe-container", {
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        568: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        968: {
+            slidesPerView: 5,
+        },
+  
+    }
+  });
+  
+  
+  // BACK BUTTON
+  
+  const backButton = document.querySelector('.back-button');
+  
+  backButton.addEventListener('click', () => {
+  
+  window.history.back();
+  });
